549795202db7f22b3772d4d565800080375fd834
Patrick <patrick.x.rivera@gmail.com>
2018-01-26 16:24:18 -0500

    Updated server

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname)));
-app.use('/src/styles', express.static(__dirname));
-app.use('/src', express.static(__dirname + '/src'));
+app.use(express.static(path.join(__dirname, '../src')));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
